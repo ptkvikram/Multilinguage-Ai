6ad76a98caf5f7d937b36431b69b1a0f7d3c5e68
vikram singh <vikramgreet@gmail.com>
2024-05-21 13:13:45 +0530

    updated

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+from src.helper import voice_input, text_to_speech, llm_model_object
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import wikipedia 
+import streamlit
+import pipwin
+import gTTS
+import google-generativeai as genia
+import Pyaudio 
+import SpeechRecognition
